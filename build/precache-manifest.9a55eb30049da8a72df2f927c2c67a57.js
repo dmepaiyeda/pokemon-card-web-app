3d76eefb7cf56767fb94da64b2a6df3781b6d445
Deborah Mepaiyeda <deborahmepaiyeda@Deborahs-MacBook-Pro.local>
2020-09-11 16:13:24 -0400

    fixing up app build

---
+++ build/precache-manifest.9a55eb30049da8a72df2f927c2c67a57.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b559ca57462cebba9f4d06f10410ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85a40b1279eb0e97fb5c",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "35876a8fbd9b0d3baa5a",
+    "url": "/static/css/main.f0cbda49.chunk.css"
+  },
+  {
+    "revision": "85a40b1279eb0e97fb5c",
+    "url": "/static/js/2.1a87d804.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1a87d804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35876a8fbd9b0d3baa5a",
+    "url": "/static/js/main.677dacd2.chunk.js"
+  },
+  {
+    "revision": "f4e530b0101a631d3b51",
+    "url": "/static/js/runtime-main.0044fc6f.js"
+  },
+  {
+    "revision": "aebaf22b197b191817fd5b1655b979bb",
+    "url": "/static/media/pokemon.aebaf22b.jpg"
+  }
+]);
